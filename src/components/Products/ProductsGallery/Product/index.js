5d38616fd0d7d183f0addd56c5a946edba5ac9db
Mary Tyutyunnik <mary.tyutyunnik@gmail.com>
2019-02-09 19:30:30 +0200

    Changes in Products category

--- src/components/Products/ProductsGallery/Product/index.js
+++ src/components/Products/ProductsGallery/Product/index.js
@@ -7,16 +7,18 @@ class Product extends Component {
         console.log(this.props);
         return (
             <>
-                <div className='productItem'>
-                    <a href="#" className="productItemImg__link">
-                        <img src={this.props.imgSrc}
-                             alt='Dress'
-                             className='productItemImg__img'/>
-                    </a>
-                    <a href="#" className="productItemTitle__link">
-                        <span className="productItemTitle__title">{this.props.title}</span>
-                    </a>
-                    <span className="productItemPrice">{this.props.price}</span>
+                <div className="productItemContent">
+                    <div className='productItem'>
+                        <a href="#" className="productItemImg__link">
+                            <img src={this.props.imgSrc}
+                                 alt='Dress'
+                                 className='productItemImg__img'/>
+                        </a>
+                        <a href="#" className="productItemTitle__link">
+                            <span className="productItemTitle__title">{this.props.title}</span>
+                        </a>
+                        <span className="productItemPrice">{this.props.price}</span>
+                    </div>
                 </div>
             </>
         );
