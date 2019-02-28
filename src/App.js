946129d9566b4eb8c3b1572e23f80bccd1b4a7ec
Dennis Baklan <dennis.baklan@gmail.com>
2019-02-28 20:24:36 +0200

    Local App.js additon(does not go to remote!)

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Homepage from './pages/Homepage/Homepage.js'
+import HomepageGender from './pages/HomepageGender/HomepageGender.js';
+import Account from './pages/Account/Account.js';
+import ProductSingle from './pages/ProductSingle/ProductSingle.js';
+import MyCart from './pages/MyCart/MyCart.js'
+
+import './App.scss';
+
+class App extends Component {
+
+    render() {
+        return (
+            <>
+                <Switch>
+                    <Route exact path="/" component={Homepage}/>
+                    <Route exact path="/:department" component={HomepageGender}/>
+                    <Route exact path="/:department/:id" component={ProductSingle}/>
+                    <Route exact path="/login" component={Account}/>
+                    <Route exact path="/basket" component={MyCart}/>
+                </Switch>
+            </>
+        );
+    }
+}
+
+export default App;
