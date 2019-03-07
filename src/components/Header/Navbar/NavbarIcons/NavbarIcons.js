4162df56cb9684934b89bc1e168c1846ad85aee4
gen-a <pugachevsky@gmail.com>
2019-03-07 19:59:01 +0200

    Add on add to cart animation and icon counter

--- src/components/Header/Navbar/NavbarIcons/NavbarIcons.js
+++ src/components/Header/Navbar/NavbarIcons/NavbarIcons.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import { FaRegUser } from 'react-icons/fa';
 import { NavLink } from 'react-router-dom';
 import AddProductToCartIcon from '../../../AddProductToCartIcon/AddProductToCartIcon';
@@ -7,27 +9,54 @@ import Search from '../Search/Search';
 
 import './NavbarIcons.scss';
 
-const NavbarIcons = () => {
-    return (
-        <>
-            <Search/>
-            <ul className="navbarIcons">
-                <li className="navbarIcons__item">
-                    <NavLink to="/login" className="navbarIcons__link">
-                        <FaRegUser className="navbarIcons__icon"/>
-                    </NavLink>
-                </li>
-                <li className="navbarIcons__item">
-                    <NavLink to="/cart" className="navbarIcons__link navbarIcons__cartLink">
-                        <AddProductToCartIcon className="navbarIcons__icon addProductToCartIcon"/>
-                        <div className="productsQuantity">
-                            <span className="productsQuantity__number">3</span>
-                        </div>
-                    </NavLink>
-                </li>
-            </ul>
-        </>
-    )
+
+const propTypes = {
+  cartCount: PropTypes.number
 };
 
-export default NavbarIcons;
+const defaultProps = {
+  cartCount: 0
+};
+
+
+const NavbarIcons = ({ cartCount }) => {
+  return (
+    <>
+    <Search/>
+    <ul className="navbarIcons">
+      <li className="navbarIcons__item">
+        <NavLink to="/login" className="navbarIcons__link">
+          <FaRegUser className="navbarIcons__icon"/>
+        </NavLink>
+      </li>
+      <li className="navbarIcons__item navbarIcons__buttonBox">
+        {cartCount > 0 && (
+          <NavLink to="/cart" className="navbarIcons__link navbarIcons__cartLink" id="shoppingCartIcon">
+            <AddProductToCartIcon className="navbarIcons__icon addProductToCartIcon"/>
+            <div className="productsQuantity">
+              <span className="productsQuantity__number navbarIcons__cartQuantity">{cartCount}</span>
+            </div>
+          </NavLink>
+        )}
+        {cartCount === 0 && (
+          <span style={{ opacity:0.2 }}>
+            <AddProductToCartIcon className="navbarIcons__icon" />
+          </span>
+        )}
+        
+
+
+      </li>
+    </ul>
+    </>
+  )
+};
+
+NavbarIcons.propTypes = propTypes;
+NavbarIcons.defaultProps = defaultProps;
+
+const mapStateToProps = state => ({
+  cartCount: state.cart.count
+});
+
+export default connect(mapStateToProps, null)(NavbarIcons);
--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -5,11 +5,12 @@ import {
 
 const initialState = {
   products: [],
-  total: 0
+  total: 0,
+  count: 0
 };
 
 /**
- * Calculate totla sum of the products
+ * Calculate total sum of the products
  * @param products
  * @returns {number}
  */
@@ -20,13 +21,25 @@ const getTotal = (products) => {
   return total;
 };
 
+/**
+ * Calculate number of the products
+ * @param products
+ * @returns {number}
+ */
+const getCount = (products) => {
+  let count = 0;
+  products.forEach(p => count += p.quantity);
+  return count;
+};
+
 const cart = (state = initialState, action) => {
   switch (action.type) {
     case UPDATE_PRODUCTS_IN_CART:
       return {
         ...state,
         products: action.payload,
-        total: getTotal(action.payload)
+        total: getTotal(action.payload),
+        count: getCount(action.payload),
       };
     default:
       return state;
