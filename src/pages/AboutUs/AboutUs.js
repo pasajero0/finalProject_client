f5800e3b7bf435590afc0c331662f71585348f84
Dennis Baklan <dennis.baklan@gmail.com>
2019-03-25 18:28:28 +0200

    Adding Helmet

--- src/pages/AboutUs/AboutUs.js
+++ src/pages/AboutUs/AboutUs.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
 import './AboutUs.scss'
+import Helmet from '../../components/SeoHeaderRender/SeoHeaderRender'
 
 
 
