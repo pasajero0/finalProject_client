35fa76ba19e4e14bf0948b244a61d186799c0ec0
Mary Tyutyunnik <frick.ua@gmail.com>
2019-03-26 16:42:03 +0200

    Deleted placeholder in forms

--- src/components/Forms/RenderField/RenderField.js
+++ src/components/Forms/RenderField/RenderField.js
@@ -5,7 +5,7 @@ const RenderField = ({ input, type, label, meta: { touched, error, warning } }) 
   return (
     <div className="renderField">
       <p className="renderField__label">{label}</p>
-      <input className="renderField__input" {...input} placeholder={label} type={type}/>
+      <input className="renderField__input" {...input} type={type}/>
       <p className="renderField__error">{touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}</p>
     </div>
   );
