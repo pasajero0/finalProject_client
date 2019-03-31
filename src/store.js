ecdc5a6d3594a3353a60babb30b230306428bbe0
gen-a <pugachevsky@gmail.com>
2019-03-31 17:27:51 +0300

    Fix store.js

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import {createStore, applyMiddleware, compose} from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers/index';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
@@ -7,10 +7,13 @@ const DEV = process.env.NODE_ENV !== 'production';
 
 const middleware = DEV ? [thunk, logger] : [thunk];
 
-const composeEnhancers = DEV ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose;
+const composeEnhancers = DEV && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  : compose;
+
 const store = createStore(
-    rootReducer,
-    composeEnhancers(applyMiddleware(...middleware))
+  rootReducer,
+  composeEnhancers(applyMiddleware(...middleware))
 );
 
 export default store;
