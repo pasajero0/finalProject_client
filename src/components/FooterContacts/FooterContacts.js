bb8341457fc9fabbbe8b21af508c0228332a3570
Mary Tyutyunnik <mary.tyutyunnik@gmail.com>
2019-04-01 00:47:19 +0300

    Changes in styles + adaptation fix

--- src/components/FooterContacts/FooterContacts.js
+++ src/components/FooterContacts/FooterContacts.js
@@ -7,27 +7,29 @@ import './FooterContacts.scss';
 const FooterContacts = () => {
   return (
     <>
-      <footer className="footer">
+      <footer className="footerContacts">
         <div className="container">
-          <div className="footer__content">
-            <div className="footer__map"><Map/></div>
-            <div className="footer__contacts">
-              <p className="footer__logo">
+          <div className="footerContacts__content">
+            <div className="footerContacts__map">
+              <Map />
+            </div>
+            <div className="footerContacts__contacts">
+              <p className="footerContacts__logo">
               </p>
-              <p className="footer__address">
+              <p className="footerContacts__address">
                 69-25 Harrison Str<br />
                 New York, NY 100-13
               </p>
 
-              <p className="footer__phone">
-                <a className="phone__link" href="tel:1(212)1234567">
+              <p className="footerContacts__phone">
+                <a className="footerContacts__phoneLink" href="tel:1(212)1234567">
                   <FaPhone />
                   <span>1 (212) 123 45 67</span>
                 </a>
               </p>
 
-              <p className="footer__email">
-                <a className="email__link" href="mailto:welcome@unostore.com">
+              <p className="footerContacts__email">
+                <a className="footerContacts__emailLink" href="mailto:welcome@unostore.com">
                   <FaEnvelope />
                   <span>welcome@unostore.com</span>
                 </a>
